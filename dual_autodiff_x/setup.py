5fe0c492c13a878a6040a0812bb7bf115df2157b
Raunaq <raunaq.rai@hotmail.co.uk>
2024-11-22 11:12:17 +0000

    preparing to create wheel

--- dual_autodiff_x/setup.py
+++ dual_autodiff_x/setup.py
@@ -1,22 +1,25 @@
-from setuptools import setup, Extension
+from setuptools import setup, Extension, find_packages
 from Cython.Build import cythonize
 
-# Cythonize your modules
-cython_modules = [
-    Extension("dual_autodiff.dual", ["dual_autodiff/dual.py"]),
-    Extension("dual_autodiff.functions", ["dual_autodiff/functions.py"]),
-    Extension("dual_autodiff.version", ["dual_autodiff/version.py"]),
+# Define Cython extensions, linking to the C source files
+extensions = [
+    Extension("dual_autodiff.dual", ["dual_autodiff/dual.c"]),
+    Extension("dual_autodiff.functions", ["dual_autodiff/functions.c"]),
+    Extension("dual_autodiff.version", ["dual_autodiff/version.c"]),
 ]
 
+# Setup configuration
 setup(
     name="dual_autodiff_x",
     version="0.1.0",
     author="Raunaq Rai",
-    packages=["dual_autodiff"],
+    description="A package for automatic differentiation using dual numbers.",
+    packages=find_packages(exclude=["tests*", "docs*", "*.pyx"]),  # Exclude `.pyx` files
     ext_modules=cythonize(
-        cython_modules,
-        compiler_directives={"language_level": "3"}  # Python 3 compatibility
+        extensions,
+        compiler_directives={"language_level": "3"},  # Python 3 language level
     ),
-    zip_safe=False,
+    zip_safe=False,  # Required for extensions
+    include_package_data=True,  # Include additional non-Python files
 )
 
