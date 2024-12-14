bb14143ff41b3ab874ca7ae1646749b084b82f03
Raunaq <raunaq.rai@hotmail.co.uk>
2024-12-14 15:33:23 +0000

    use of old laptop to generate wheels

---
+++ dual_autodiff_x/wheel_contents/310_wheel_contents/dual_autodiff_x/__init__.py
@@ -0,0 +1,17 @@
+from .dual import Dual
+from .functions import (
+    sin, cos, tan, log, exp, sqrt,
+    sinh, cosh, tanh, asin, acos, atan
+)
+
+# Define the version directly here
+__version__ = "0.1.16"
+
+# Define the public API
+__all__ = [
+    "__version__",  # Expose version information
+    "Dual",         # Expose the Dual class
+    "sin", "cos", "tan", "log", "exp", "sqrt",  # Expose functions
+    "sinh", "cosh", "tanh", "asin", "acos", "atan"
+]
+
